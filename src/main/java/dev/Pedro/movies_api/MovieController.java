37dd9358cc95f363172e5337c970404478cc98d4
Pedro Silva <pedrosilva222004@gmail.com>
2025-07-07 16:49:29 +0100

    Simple Movie Endpoints (GET all and single movue) done and tested.

--- src/main/java/dev/Pedro/movies_api/MovieController.java
+++ src/main/java/dev/Pedro/movies_api/MovieController.java
@@ -1,11 +1,13 @@
 package dev.Pedro.movies_api;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +21,11 @@ public class MovieController {
     @GetMapping
     public ResponseEntity<List<Movie>> getAllMovies() {
         return new ResponseEntity<>(movieService.AllMovies(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{imdbId}")
+    public ResponseEntity<Optional<Movie>> getMovie(@PathVariable String imdbId) {
+        return new ResponseEntity<>(movieService.singleMovie(imdbId), HttpStatus.OK);
     }
 
 }
--- src/main/java/dev/Pedro/movies_api/MovieRepository.java
+++ src/main/java/dev/Pedro/movies_api/MovieRepository.java
@@ -1,10 +1,12 @@
 package dev.Pedro.movies_api;
 
+import java.util.Optional;
+
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
-
+    public Optional<Movie> findMovieByImdbId(String imdbId);
 }
