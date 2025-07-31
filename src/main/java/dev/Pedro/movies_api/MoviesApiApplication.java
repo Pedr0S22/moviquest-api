c7195d92c21819c962a9bd2208c8e60e4f7dc7bb
Pedro Silva <pedrosilva222004@gmail.com>
2025-07-31 15:52:33 +0100

    Updated MoviesApiApplication Javadocs

--- src/main/java/dev/Pedro/movies_api/MoviesApiApplication.java
+++ src/main/java/dev/Pedro/movies_api/MoviesApiApplication.java
@@ -4,9 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * <p>
+ *
  * Main entry point for the Spring Boot MovieQuest Application.
- * </p>
  *
  * <p>
  * This class is responsible for bootstrapping the Spring application context
