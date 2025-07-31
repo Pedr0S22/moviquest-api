8cd2a1bc116df660b479e3cd7e26c7af9266e118
Pedro Silva <pedrosilva222004@gmail.com>
2025-07-31 17:06:05 +0100

    LogEventRepository javadocs created

--- src/main/java/dev/Pedro/movies_api/repository/LogEventRepository.java
+++ src/main/java/dev/Pedro/movies_api/repository/LogEventRepository.java
@@ -6,6 +6,15 @@ import org.springframework.stereotype.Repository;
 
 import dev.Pedro.movies_api.model.LogEvent;
 
+/**
+ * Repository interface for accessing {@link LogEvent} documents in MongoDB.
+ * <p>
+ * Extends Spring Data's {@link MongoRepository} to provide CRUD operations
+ * and additional MongoDB-specific functionalities for {@link LogEvent}
+ * entities.
+ * The primary key type is {@link ObjectId}.
+ * </p>
+ */
 @Repository
 public interface LogEventRepository extends MongoRepository<LogEvent, ObjectId> {
 
