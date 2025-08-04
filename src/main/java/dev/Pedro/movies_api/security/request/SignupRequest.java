ecd6fe1a6d1e382dd547a892b8a6138e819f7ff9
Pedro Silva <pedrosilva222004@gmail.com>
2025-08-04 16:15:47 +0100

    Add DTOs for login and signup

---
+++ src/main/java/dev/Pedro/movies_api/security/request/SignupRequest.java
@@ -0,0 +1,33 @@
+package dev.Pedro.movies_api.security.request;
+
+import java.util.Set;
+
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SignupRequest {
+    @NotBlank
+    @Size(min = 5, max = 20)
+    private String username;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(min = 8, max = 40)
+    private String password;
+
+    @DocumentReference
+    @NotBlank
+    private Set<String> roles;
+}
---
+++ src/main/java/dev/Pedro/movies_api/security/response/JwtResponse.java
@@ -0,0 +1,28 @@
+package dev.Pedro.movies_api.security.response;
+
+import java.util.Set;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class JwtResponse {
+
+    private String token;
+    private String type = "Bearer";
+    private String id;
+    private String username;
+    private String email;
+    private Set<String> roles;
+
+    public JwtResponse(String accessToken, String id, String username, String email, Set<String> roles) {
+        this.token = accessToken;
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.roles = roles;
+    }
+}
