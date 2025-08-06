7403d7fa8f23bc34a73036a52308d630ed4d781e
Pedro Silva <pedrosilva222004@gmail.com>
2025-08-07 00:14:55 +0100

    Auth controller and service added. the controller only have pseudocode by now and service has pseudocode + partial signup implementation

--- src/main/java/dev/Pedro/movies_api/controller/AuthController.java
+++ src/main/java/dev/Pedro/movies_api/controller/AuthController.java
@@ -1,5 +1,47 @@
 package dev.Pedro.movies_api.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import dev.Pedro.movies_api.security.request.LoginRequest;
+import dev.Pedro.movies_api.security.request.SignupRequest;
+import dev.Pedro.movies_api.service.AuthService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
+@Slf4j
+@RequestMapping("/api/v1/auth")
 public class AuthController {
 
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Object> handleLogin(@RequestBody LoginRequest login) {
+
+        // Se o login corresponder enviar JwtResponse
+
+        return ResponseEntity.ok(null);
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<Object> handleSignup(@RequestBody SignupRequest signup) {
+
+        // verificar se já existe username
+
+        // verificar se já existe email
+
+        // Criar objeto User e Role e inserir na base de dados
+        // encode da password
+
+        // Enviar a confirmação de criação da conta
+
+        return ResponseEntity.ok(null);
+    }
 }
---
+++ src/main/java/dev/Pedro/movies_api/service/AuthService.java
@@ -0,0 +1,103 @@
+package dev.Pedro.movies_api.service;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import dev.Pedro.movies_api.exception.EmailAlreadyExistsException;
+import dev.Pedro.movies_api.exception.RoleNotFoundException;
+import dev.Pedro.movies_api.exception.UsernameAlreadyExistsException;
+import dev.Pedro.movies_api.model.ClientRoles;
+import dev.Pedro.movies_api.model.Role;
+import dev.Pedro.movies_api.model.User;
+import dev.Pedro.movies_api.repository.RoleRepository;
+import dev.Pedro.movies_api.repository.UserRepository;
+import dev.Pedro.movies_api.security.request.LoginRequest;
+import dev.Pedro.movies_api.security.request.SignupRequest;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+public class AuthService {
+
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder encoder;
+
+    public AuthService(UserRepository userRepository, RoleRepository roleRepository, PasswordEncoder encoder) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.encoder = encoder;
+    }
+
+    public ResponseEntity<Object> handleLoginService(@Valid @RequestBody LoginRequest login) {
+
+        // fazer autenticação
+
+        // Verificar na base de dados se está tudo correto
+
+        // Atribuir o token se sim e enviar responseBody
+        return ResponseEntity.ok(null);
+    }
+
+    public ResponseEntity<Object> handleSignupService(@Valid @RequestBody SignupRequest signup) {
+
+        // Verify if username exists in database
+        String username = signup.getUsername();
+        if (!userRepository.existsByUsername(username)) {
+            log.debug("The username {} is already taken", username);
+            throw new UsernameAlreadyExistsException("The username is already taken");
+        }
+
+        // verify if email exists in database
+        String email = signup.getEmail();
+        if (!userRepository.existsByEmail(email)) {
+            log.debug("The email {} is already taken", email);
+            throw new EmailAlreadyExistsException("The email is already taken");
+        }
+
+        // User object creation with encoded password
+        User user = new User(username,
+                email,
+                encoder.encode(signup.getPassword()));
+
+        // Verification of the existence of client roles
+        Set<String> strRoles = signup.getRoles();
+        Set<Role> roles = new HashSet<>();
+
+        if (strRoles == null) {
+            Optional<Role> userRole = roleRepository.findByRoleName(ClientRoles.USER_ROLE);
+            if (userRole.isPresent()) {
+                roles.add(userRole.get());
+            } else {
+                log.debug("Role {} does not exist in database", userRole);
+                throw new RoleNotFoundException("Role " + userRole + " does not exist in database");
+            }
+        } else {
+            strRoles.forEach(role -> {
+                switch (role) {
+                    case "admin":
+
+                        break;
+
+                    default:
+                        break;
+                }
+            });
+
+        }
+
+        // Append roles into User object
+        user.setRoles(roles);
+
+        // Enviar a confirmação de criação da conta
+
+        return ResponseEntity.ok(null);
+    }
+}
