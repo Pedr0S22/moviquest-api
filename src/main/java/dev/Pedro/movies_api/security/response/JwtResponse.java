74dc25012480456252935962037ee5ebe82be347
Pedro Silva <pedrosilva222004@gmail.com>
2025-08-07 00:11:24 +0100

    JwtResponse DTO changed to support signup and login responses

--- src/main/java/dev/Pedro/movies_api/security/response/JwtResponse.java
+++ src/main/java/dev/Pedro/movies_api/security/response/JwtResponse.java
@@ -2,6 +2,8 @@ package dev.Pedro.movies_api.security.response;
 
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,6 +11,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class JwtResponse {
 
     private String token;
@@ -18,11 +21,22 @@ public class JwtResponse {
     private String email;
     private Set<String> roles;
 
+    private String message;
+
     public JwtResponse(String accessToken, String id, String username, String email, Set<String> roles) {
         this.token = accessToken;
         this.id = id;
         this.username = username;
         this.email = email;
         this.roles = roles;
+    }
+
+    public JwtResponse(String id, String username, String email, Set<String> roles, String message) {
+
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.roles = roles;
+        this.message = message;
     }
 }
