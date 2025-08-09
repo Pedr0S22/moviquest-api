785acc1af94d42782f1e47df79841940d3cfb0df
Pedro Silva <pedrosilva222004@gmail.com>
2025-08-09 13:42:50 +0100

    Added dependecy to handle LocalTimeDate objects serialization and a bean with an ObjectMapper to do the job

---
+++ src/main/java/dev/Pedro/movies_api/security/configuration/JacksonConfig.java
@@ -0,0 +1,19 @@
+package dev.Pedro.movies_api.security.configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+
+@Configuration
+public class JacksonConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        return mapper;
+    }
+}
