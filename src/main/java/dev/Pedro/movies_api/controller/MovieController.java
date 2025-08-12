e6ec53286d3e9c935e63b13508368614547007f5
Pedro Silva <pedrosilva222004@gmail.com>
2025-08-12 15:32:21 +0100

    Added endpoint in controller to delete a movie with imbdId. This endpoint, to be used, needs admin auth

--- src/main/java/dev/Pedro/movies_api/controller/MovieController.java
+++ src/main/java/dev/Pedro/movies_api/controller/MovieController.java
@@ -3,12 +3,15 @@ package dev.Pedro.movies_api.controller;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import dev.Pedro.movies_api.dto.request.SearchMoviesRequest;
+import dev.Pedro.movies_api.dto.response.ApiResponse;
 import dev.Pedro.movies_api.model.Movie;
 import dev.Pedro.movies_api.service.MovieService;
 import jakarta.servlet.http.HttpServletRequest;
@@ -38,7 +41,7 @@ public class MovieController {
     }
 
     @GetMapping("/{imdbId}")
-    public ResponseEntity<Movie> getMovie(@PathVariable String imdbId, HttpServletRequest request) {
+    public ResponseEntity<Movie> getMovie(@PathVariable String imdbId) {
         log.info("Received request to get the movie with imdbId {}", imdbId);
         Movie movie = movieService.singleMovie(imdbId);
         log.info("The movie with imdbId {} was provided", imdbId);
@@ -55,6 +58,17 @@ public class MovieController {
         log.info("Returning {} movies.", movies.size());
 
         return ResponseEntity.ok(movies);
+    }
+
+    @DeleteMapping("/delete/{imdbId}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<ApiResponse> deleteMovie(@PathVariable String imdbId, HttpServletRequest request) {
+
+        log.info("Received request to delete the movie with imdbId {}", imdbId);
+        ApiResponse deleteResponse = movieService.deleteMovieByImdbId(imdbId, request);
+        log.info("The movie with imdbId {} was deleted", imdbId);
+
+        return ResponseEntity.ok(deleteResponse);
     }
 
 }
