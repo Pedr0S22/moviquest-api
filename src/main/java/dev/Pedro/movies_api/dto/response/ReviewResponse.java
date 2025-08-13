806acc77a18812108eac6b0e627e83abd24a680f
Pedro Silva <pedrosilva222004@gmail.com>
2025-08-13 12:33:41 +0100

    ReviewResponse contructor modification

--- src/main/java/dev/Pedro/movies_api/dto/response/ReviewResponse.java
+++ src/main/java/dev/Pedro/movies_api/dto/response/ReviewResponse.java
@@ -1,18 +1,19 @@
 package dev.Pedro.movies_api.dto.response;
 
 import dev.Pedro.movies_api.model.Review;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class ReviewResponse {
 
     private int status;
     private String message;
     private Review review;
 
-    public ReviewResponse(int status, String message, Review review) {
+    public ReviewResponse(int status, String message) {
         this.status = status;
         this.message = message;
-        this.review = review;
     }
 }
