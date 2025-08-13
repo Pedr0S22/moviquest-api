90542b4afbcc21b6bc28ef2c0f7913e1f6c61e6a
Pedro Silva <pedrosilva222004@gmail.com>
2025-08-13 16:06:40 +0100

    Reverted last modifications on ReviewResponse (Deleting all agrs construtor) and added manually with localdatetime

--- src/main/java/dev/Pedro/movies_api/dto/response/ReviewResponse.java
+++ src/main/java/dev/Pedro/movies_api/dto/response/ReviewResponse.java
@@ -1,19 +1,29 @@
 package dev.Pedro.movies_api.dto.response;
 
+import java.time.LocalDateTime;
+
 import dev.Pedro.movies_api.model.Review;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-@AllArgsConstructor
 public class ReviewResponse {
 
     private int status;
     private String message;
     private Review review;
 
+    private LocalDateTime localDateTime;
+
     public ReviewResponse(int status, String message) {
         this.status = status;
         this.message = message;
+        this.localDateTime = LocalDateTime.now();
+    }
+
+    public ReviewResponse(int status, String message, Review review) {
+        this.status = status;
+        this.message = message;
+        this.localDateTime = LocalDateTime.now();
+        this.review = review;
     }
 }
