3f1a4570924447a821ecd3f37e60a7ff2021953f
Pedro Silva <pedrosilva222004@gmail.com>
2025-08-13 15:47:16 +0100

    Added business logic for deleting review. the ReviewSecurity is also implemented to prevent other clients accesing this endpoint

--- src/main/java/dev/Pedro/movies_api/service/ReviewService.java
+++ src/main/java/dev/Pedro/movies_api/service/ReviewService.java
@@ -1,8 +1,11 @@
 package dev.Pedro.movies_api.service;
 
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import dev.Pedro.movies_api.model.Movie;
@@ -10,11 +13,16 @@ import dev.Pedro.movies_api.model.Review;
 import dev.Pedro.movies_api.dto.request.NewReviewRequest;
 import dev.Pedro.movies_api.exception.*;
 import dev.Pedro.movies_api.repository.ReviewRepository;
+import dev.Pedro.movies_api.security.service.UserDetailsImpl;
 import lombok.extern.slf4j.Slf4j;
+
+import dev.Pedro.movies_api.repository.MovieRepository;
 
 @Service
 @Slf4j
 public class ReviewService {
+
+    private final MovieRepository movieRepository;
 
     private final ReviewRepository reviewRepository;
 
@@ -22,10 +30,12 @@ public class ReviewService {
 
     private final MongoTemplate mongoTemplate;
 
-    public ReviewService(ReviewRepository reviewRepository, MovieService movieService, MongoTemplate mongoTemplate) {
+    public ReviewService(ReviewRepository reviewRepository, MovieService movieService, MongoTemplate mongoTemplate,
+            MovieRepository movieRepository) {
         this.reviewRepository = reviewRepository;
         this.movieService = movieService;
         this.mongoTemplate = mongoTemplate;
+        this.movieRepository = movieRepository;
     }
 
     public Review createReview(NewReviewRequest newReview) {
@@ -39,7 +49,12 @@ public class ReviewService {
             throw new MovieNotFoundException(errorMessage);
         }
 
-        Review review = reviewRepository.insert(new Review(newReview.getBody()));
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        UserDetailsImpl principal = (UserDetailsImpl) auth.getPrincipal();
+
+        String currentUsername = principal.getUsername();
+
+        Review review = reviewRepository.insert(new Review(newReview.getBody(), currentUsername));
 
         mongoTemplate.update(Movie.class)
                 .matching(Criteria.where("imdbId").is(newReview.getImdbId()))
@@ -47,5 +62,32 @@ public class ReviewService {
                 .first();
 
         return review;
+    }
+
+    public void deleteReview(String imdbId, String id) {
+
+        Movie movie = movieService.singleMovie(imdbId);
+
+        if (verifyReviewExistence(id)) {
+
+            log.debug("antes");
+            // verify if that review exists in that movie and removes it
+            boolean removed = movie.getReviewIds().removeIf(review -> review.getId().toHexString().equals(id));
+            log.debug("depois");
+
+            if (!removed)
+                throw new ReviewNotFoundException(
+                        "Review with id " + id + " not found from movie with imdbId " + imdbId);
+
+            movieRepository.save(movie);
+            reviewRepository.deleteById(new ObjectId(id));
+        } else
+            throw new ReviewNotFoundException(
+                    "Review with id " + id + " not found");
+
+    }
+
+    private boolean verifyReviewExistence(String id) {
+        return reviewRepository.existsById(new ObjectId(id));
     }
 }
