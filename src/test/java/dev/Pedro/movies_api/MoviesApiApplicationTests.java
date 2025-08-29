3836b2798a2128974afc82bee45c45b42764ded0
Pedro Silva <pedrosilva222004@gmail.com>
2025-08-29 16:07:24 +0100

    Added container tests using mongo and junit for testing only and modified the main app test file with the static container

--- src/test/java/dev/Pedro/movies_api/MoviesApiApplicationTests.java
+++ src/test/java/dev/Pedro/movies_api/MoviesApiApplicationTests.java
@@ -2,9 +2,23 @@ package dev.Pedro.movies_api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class MoviesApiApplicationTests {
+
+	@Container
+	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:7.0");
+
+	@DynamicPropertySource
+	static void setProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+	}
 
 	@Test
 	void contextLoads() {
