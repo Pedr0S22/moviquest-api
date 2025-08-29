0b68df21a909ac7ff3ac9abb452c546233a7ea78
Pedro Silva <pedrosilva222004@gmail.com>
2025-08-29 19:37:10 +0100

    only run build in linux ci.yml and add more protection in mongo container

--- src/test/java/dev/Pedro/movies_api/MoviesApiApplicationTests.java
+++ src/test/java/dev/Pedro/movies_api/MoviesApiApplicationTests.java
@@ -1,6 +1,9 @@
 package dev.Pedro.movies_api;
 
+import java.time.Duration;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
@@ -9,12 +12,16 @@ import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
-
 @Testcontainers
+@EnableAutoConfiguration(exclude = {
+		org.springframework.boot.actuate.autoconfigure.audit.AuditEventsEndpointAutoConfiguration.class
+})
 class MoviesApiApplicationTests {
 
 	@Container
-	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:7.0");
+	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:7.0")
+			.withStartupAttempts(3)
+			.withStartupTimeout(Duration.ofSeconds(60));
 
 	@DynamicPropertySource
 	static void setProperties(DynamicPropertyRegistry registry) {
