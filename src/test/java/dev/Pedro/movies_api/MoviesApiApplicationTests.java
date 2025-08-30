de8a642a5c68632df090b7f7af1e7a924882e317
Pedro Silva <pedrosilva222004@gmail.com>
2025-08-30 13:59:51 +0100

    changed MoviesApiApplicationTests to the version that made the build in github actions

--- src/test/java/dev/Pedro/movies_api/MoviesApiApplicationTests.java
+++ src/test/java/dev/Pedro/movies_api/MoviesApiApplicationTests.java
@@ -1,19 +1,21 @@
 package dev.Pedro.movies_api;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
-@Testcontainers(disabledWithoutDocker = true)
+@DataMongoTest
+@Testcontainers
 class MoviesApiApplicationTests {
 
 	@Container
 	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:7.0");
 
-	@Test
-	void contextLoads() {
+	@DynamicPropertySource
+	static void setProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
 	}
 }
